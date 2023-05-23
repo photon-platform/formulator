638047cac51a1836612fad6e5f8d33d9a4a33024
phiarchitect <phi@phiarchitect.com>
2023-05-23 06:37:48 -0700

    version 0.0.3

--- src/photon_platform/formulator/__init__.py
+++ src/photon_platform/formulator/__init__.py
@@ -14,5 +14,5 @@ from .formulator import *
 __author__ = "phiarchitect"
 __maintainer__ = "PHOTON platform"
 __email__ = "github@phiarchitect.com"
-__version__ = "0.0.2"
+__version__ = "0.0.3"
 __licence__ = "MIT"
